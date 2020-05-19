d1e05efe304d4581b6d3076d47de230ba2a4856a
Boss <bossnathakorn@gmail.com>
2020-05-20 00:14:28 +0700

    L05

---
+++ Assets/Scripts/CandyScript.cs
@@ -0,0 +1,13 @@
+﻿using Constant;
+using UnityEngine;
+
+public class CandyScript : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag(Tag.Player))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Constant/Tag.cs
@@ -0,0 +1,8 @@
+﻿namespace Constant
+{
+    public struct Tag
+    {
+        public static string Player = "Player";
+        public static string Candy = "Candy";
+    }
+}
