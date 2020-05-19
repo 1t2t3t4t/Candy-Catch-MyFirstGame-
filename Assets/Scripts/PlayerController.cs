56fd6961dd29325f17fd55c75e5647dc3e0e78ef
Boss <bossnathakorn@gmail.com>
2020-05-19 23:58:44 +0700

    L03

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float MoveSpeed;
+    
+    private bool _canMove = true;
+
+    private void Update()
+    {
+        if (_canMove)
+        {
+            _move();
+        }
+    }
+
+    private void _move()
+    {
+        var xMovement = Input.GetAxis("Horizontal");
+        transform.position += Vector3.right * (xMovement * MoveSpeed * Time.deltaTime); 
+    }
+}
